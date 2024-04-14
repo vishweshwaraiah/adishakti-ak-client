3aede7806dcea0b36eee0549d4129d31a31f2613
Vishweshwarayya Kj <k.vishu@outlook.com>
2024-04-14 11:50:47 +0530

    AK-Client: Added server url

--- app/utils/Globals.js
+++ app/utils/Globals.js
@@ -2,7 +2,7 @@ import Constants from 'expo-constants';
 import { phoneRegex, clearRegex } from '@/utils/AppConstants';
 export const ServerUri = Constants?.expoConfig?.hostUri
   ? 'http://' + Constants.expoConfig.hostUri.split(`:`).shift().concat(`:3000`)
-  : `adishakti-kkmr.com`;
+  : `http://13.126.248.37/api`;
 
 export const ValidNumber = (num) => {
   const clearNum = num.replace(clearRegex, '');
